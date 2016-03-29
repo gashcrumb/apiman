9bd031b10c5c61fcccae096e41c20364c47756de
kahboom <rachelyordan@gmail.com>
2016-03-29 19:29:00 -0400

    Switch to webpack

---
+++ devportal/war/src/app/app.component.ts
@@ -0,0 +1,59 @@
+/*
+ * Angular 2 decorators and services
+ */
+import {Component} from 'angular2/core';
+import {ViewEncapsulation} from 'angular2/core';
+import {RouteConfig, Router} from 'angular2/router';
+
+import {Home} from './home';
+import {AppState} from './app.service';
+import {Dashboard} from './dashboard';
+import {RouterActive} from './router-active';
+
+/*
+ * App Component
+ * Top Level Component
+ */
+@Component({
+    selector: 'app',
+    pipes: [],
+    providers: [],
+    directives: [RouterActive],
+    styles: [
+        require('../assets/sass/main.scss')
+    ],
+    encapsulation: ViewEncapsulation.None,
+    template: require('./layout.html')
+})
+@RouteConfig([
+    {path: '/', name: 'Home', component: Home, useAsDefault: true},
+    //{path: '/home', name: 'Home', component: Home},
+    // Async load a component using Webpack's require with es6-promise-loader and webpack `require`
+    {path: '/about', name: 'About', loader: () => require('es6-promise!./about')('About')},
+    {path: '/dashboard', name: 'Dashboard', component: Dashboard}
+])
+export class App {
+    angularclassLogo = 'assets/img/angularclass-avatar.png';
+    title = 'Developer Portal';
+    url = 'https://twitter.com/apiman_io';
+
+    constructor(public appState:AppState) {
+    }
+
+    get state() {
+        return this.appState.get();
+    }
+
+    ngOnInit() {
+        console.log('Initial App State', this.state);
+    }
+
+}
+
+/*
+ * Please review the https://github.com/AngularClass/angular2-examples/ repo for
+ * more angular app examples that you may copy/paste
+ * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
+ * For help or questions please contact us at @AngularClass on twitter
+ * or our chat on Slack at https://AngularClass.com/slack-join
+ */
