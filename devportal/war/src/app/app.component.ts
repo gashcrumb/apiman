0ecd9c46df37ef3516e164e2f8e153d4d11404bf
kahboom <rachelyordan@gmail.com>
2016-04-01 16:17:14 -0400

    Add Client Apps and Browse APIs views

--- devportal/war/src/app/app.component.ts
+++ devportal/war/src/app/app.component.ts
@@ -6,8 +6,10 @@ import {ViewEncapsulation} from 'angular2/core';
 import {RouteConfig, Router} from 'angular2/router';
 
 import {Home} from './home';
+import {APIs} from './apis';
 import {AppState} from './app.service';
 import {Dashboard} from './dashboard';
+import {ClientApps} from './clientApps';
 import {RouterActive} from './router-active';
 
 /*
@@ -27,15 +29,17 @@ import {RouterActive} from './router-active';
 })
 @RouteConfig([
     {path: '/', name: 'Home', component: Home, useAsDefault: true},
-    //{path: '/home', name: 'Home', component: Home},
-    // Async load a component using Webpack's require with es6-promise-loader and webpack `require`
-    {path: '/about', name: 'About', loader: () => require('es6-promise!./about')('About')},
+    {path: '/apis', name: 'APIs', component: APIs},
+    {path: '/apps', name: 'Client Apps', component: ClientApps},
     {path: '/dashboard', name: 'Dashboard', component: Dashboard}
 ])
 export class App {
-    apimanLogo = 'assets/img/apiman_logo_inline_color.png';
+    logoWhiteBg = 'assets/img/dev-portal-logo-01.png';
+    logoDarkBg = 'assets/img/dev-portal-logo-02.png';
     title = 'Developer Portal';
     url = 'https://twitter.com/apiman_io';
+    loggedIn = true;
+    //loggedIn = false;
 
     constructor(public appState:AppState) {
     }
@@ -49,11 +53,3 @@ export class App {
     }
 
 }
-
-/*
- * Please review the https://github.com/AngularClass/angular2-examples/ repo for
- * more angular app examples that you may copy/paste
- * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
- * For help or questions please contact us at @AngularClass on twitter
- * or our chat on Slack at https://AngularClass.com/slack-join
- */
